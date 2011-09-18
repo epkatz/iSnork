777ff9034e082219ead0e32b10aa691c33b1bb67
Eli Katz <friendlyneighborhoodkatz@gmail.com>
2011-09-18 15:21:55 -0400

    get rid of no movement

--- g6/DangerAvoidance.java
+++ g6/DangerAvoidance.java
@@ -25,49 +25,52 @@ public class DangerAvoidance {
 		return false;
 	}
 
-	public LinkedList<Direction> bestDirections(Set<Observation> whatISee, Direction d, Point2D currentPosition) {
+	public LinkedList<Direction> bestDirections(Set<Observation> whatISee,
+			Direction d, Point2D currentPosition) {
 		LinkedList<Direction> newL = new LinkedList<Direction>();
 		ArrayList<Direction> directionOptions = Direction.allBut(d);
-		Direction bestDirection = null;
-		Point2D bestPoint = null;
+		ArrayList<Direction> possibleSafePlaces = new ArrayList<Direction>();
 		for (Direction nextD : directionOptions) {
 			double newPosX = currentPosition.getX() + nextD.getDx();
 			double newPosY = currentPosition.getY() + nextD.getDy();
 			Point2D newPoint = new Point2D.Double(newPosX, newPosY);
-			if (!atTheWall(newPoint) && !isLocationDangerous(whatISee, newPoint)){
-				if (bestPoint == null){
-					bestPoint = newPoint;
-					bestDirection = nextD;
-				}
-				else{
-					if (tilesAway(currentPosition, newPoint) < tilesAway(currentPosition, bestPoint)){
-						bestPoint = newPoint;
-						bestDirection = nextD;
-					}
-				}
+			if (!atTheWall(newPoint) && !isLocationDangerous(whatISee, newPoint)) {
+					possibleSafePlaces.add(nextD);
 			}
 		}
-		if (bestDirection == null){
-			Random r = new Random();
-			Direction randomDirection = directionOptions.get(r.nextInt(Direction.values().length-1));
+		if (possibleSafePlaces.isEmpty()) {
+			Direction randomDirection = null;
+			do {
+				Random r = new Random();
+				randomDirection = directionOptions.get(r.nextInt(Direction.values().length - 1));
+			} while (randomDirection == Direction.STAYPUT);
 			newL.add(randomDirection);
 			double newPosX = currentPosition.getX() + randomDirection.getDx();
 			double newPosY = currentPosition.getY() + randomDirection.getDy();
 			Point2D randomPoint = new Point2D.Double(newPosX, newPosY);
-			LinkedList<Direction> temp = bestDirections(whatISee, randomDirection, randomPoint);
-			for (Direction tmpD: temp){
+			LinkedList<Direction> temp = bestDirections(whatISee,
+					randomDirection, randomPoint);
+			for (Direction tmpD : temp) {
 				newL.add(tmpD);
 			}
 			return newL;
-		}
-		else{
-			newL.add(bestDirection);
+		} else {
+			Direction randomDirection = null;
+			if (possibleSafePlaces.size() == 1){
+				newL.add(possibleSafePlaces.remove(0));
+				return newL;
+			}
+			do {
+				Random r = new Random();
+				randomDirection = directionOptions.get(r.nextInt(possibleSafePlaces.size()));
+			} while (randomDirection == Direction.STAYPUT);
+			newL.add(randomDirection);
 			return newL;
 		}
 	}
 
 	public int tilesAway(Point2D me, Point2D them) {
-		return ((int)PathManager.computeTotalSpaces(me, them));
+		return ((int) PathManager.computeTotalSpaces(me, them));
 	}
 
 	public static boolean atBoat(Point2D p) {
@@ -79,7 +82,8 @@ public class DangerAvoidance {
 	}
 
 	public static boolean atTheWall(Point2D p) {
-		if (Math.abs(p.getX()) == NewPlayer.d || Math.abs(p.getY()) == NewPlayer.d) {
+		if (Math.abs(p.getX()) == NewPlayer.d
+				|| Math.abs(p.getY()) == NewPlayer.d) {
 			return true;
 		} else {
 			return false;
