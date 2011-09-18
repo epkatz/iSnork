a1f0286e012e8fb15150c3ba654795206bb0c474
Jonecia Keels <jkeels12@gmail.com>
2011-09-18 13:46:32 -0400

    added Creature Tracker
    Each player now has a tracker and each one has a list of all the
    species of creatures they saw.
    **need to extend it to add the unique id per species seen.

--- g6/CreatureTracker.java
+++ g6/CreatureTracker.java
@@ -1,44 +1,98 @@
 package isnork.g6;
-
+import isnork.sim.Observation;
 import isnork.sim.SeaLifePrototype;
-
 import java.util.LinkedList;
-import java.util.Set;
 
 public class CreatureTracker {
 	
 	private LinkedList<Creature> creaturesSeen;
+	private int playerID;
 	public static final int MAX_SEEN = 3;
 	
-	public CreatureTracker(Set<SeaLifePrototype> seaLifePossibilites){
-		creaturesSeen = new LinkedList<Creature>();
+	public CreatureTracker(int playerID){
+		this.creaturesSeen = new LinkedList<Creature>();
+		this.playerID = playerID;
 		
 	}
 	
-	public void seeCreature(int id, String name){
-		
+	public void addToTracker(Observation creature) {
+		Creature newCreature = new Creature(creature.getName());
+		creaturesSeen.add(newCreature);
+		System.out.print("added new creature: " + creature.getName() + "\n");
 	}
+	
+	public LinkedList<Creature> getListOfCreatures() {
+		return creaturesSeen;
+	}
+	
+	public int getPlayerID() {
+		return playerID;
+	}
+		
+	//check if creature is in the list
+	public boolean didSeeCreature(String name) {
+		for(int i = 0; i < creaturesSeen.size(); i++) {
+			Creature tempObject = creaturesSeen.get(i);
+			if(tempObject.getName().equals(name))
+				return true;
+		}
+		//if here then creature not in list
+		return false;
+	}
+	
+//	//retrieve element index of specified creature
+//	public int getCreatureIndex(String name) {
+//		for(int i = 0; i < creaturesSeen.size(); i++) {
+//			Creature tempObject = creaturesSeen.get(i);
+//			if(tempObject.creature)
+//				return i;
+//		}
+//		//error if reached here
+//		System.out.print("ERROR: inside creature tracker get method");
+//		return -1;
+//		
+//	}
+//	
+//	public void seeCreature(int id, String name){
+//		if(!didSeeCreature(name)) {
+//			//add creature to the list
+//			Creature newCreature = new Creature(name);
+//			newCreature.addID(id);			
+//			//add this to the creature tracker
+//			creaturesSeen.add(newCreature);
+//			System.out.print("added creature: " + name + "\n" );
+//		} else {
+//			int creatureIndex = getCreatureIndex(name);
+//			
+//		}
+//		
+//	}
 	
 	public class Creature {
-		LinkedList<Integer> seen;
+		LinkedList<Integer> seen; //store id of the creatures seen
 		String creature;
 		
-		public Creature(String name){
-			creature = name;
+		public Creature(String creature) {
+			this.creature = creature;
 			seen = new LinkedList<Integer>();
 		}
 		
-		public void addID(int id){
-			
+		public LinkedList<Integer> getListOfSeenIDs() {
+			return seen;
 		}
 		
-		public boolean maxedOut(){
-			if (seen.size() > MAX_SEEN){
+		public boolean isMaxedOut() {
+			if(seen.size() > MAX_SEEN)
 				return true;
-			}
-			else{
-				return false;
-			}
+			return false;
+		}
+		
+		public void addID(int id) {
+			seen.add(id);
+		}
+		
+		public String getName() {
+			return creature;
 		}
 	}
 	
